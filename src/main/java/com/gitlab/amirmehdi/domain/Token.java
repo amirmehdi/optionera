aebea3358d65ed980548cbb716ce822037218bfc
oa-user <amirmehdinaghavi@gmail.com>
2020-10-19 10:04:59 +0330

    hotfix on show-sql

--- src/main/java/com/gitlab/amirmehdi/domain/Token.java
+++ src/main/java/com/gitlab/amirmehdi/domain/Token.java
@@ -34,7 +34,6 @@ public class Token implements Serializable {
     private Broker broker;
 
     @Column(name = "created_at")
-    @Temporal(TemporalType.TIMESTAMP)
     private Instant createdAt;
 
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
