db25b416ca5f09d010a02e49af335c9b80bd4738
oa-user <amirmehdinaghavi@gmail.com>
2020-10-21 01:59:18 +0330

    remove comments in home.tsx and add package-lock.json

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,6 +1,6 @@
 import './home.scss';
 import './../../../content/css/styles-merged.css'
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {Alert, Col, Row} from 'reactstrap';
 import {Link} from "react-router-dom";
@@ -12,58 +12,6 @@ export const Home = (props: IHomeProp) => {
   const [active , setActive] = useState<number>(1);
 
   return (
-    /*    <Row>
-          <Col md="9">
-            <h2>
-              <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-            </h2>
-            <p className="lead">
-              <Translate contentKey="home.subtitle">This is your homepage</Translate>
-            </p>
-            <p>
-              <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
-            </p>
-
-            <ul>
-              <li>
-                <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-                  <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
-                </a>
-              </li>
-              <li>
-                <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-                  <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-                  <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
-                </a>
-              </li>
-              <li>
-                <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-                  <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-                  <Translate contentKey="home.link.follow">follow @jhipster on Twitter</Translate>
-                </a>
-              </li>
-            </ul>
-
-            <p>
-              <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-              <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-                Github
-              </a>
-              !
-            </p>
-          </Col>
-          <Col md="3" className="pad">
-            <span className="hipster rounded" />
-          </Col>
-        </Row>*/
     <div>
       <div>
         {account && account.login ? (
