29b68be3dfe1b494d33bc739628a6c8fd43a5605
oa-user <amirmehdinaghavi@gmail.com>
2020-10-24 17:18:55 +0330

    fix time mapping in ObjectMapperConfiguration

---
+++ src/main/java/com/gitlab/amirmehdi/config/ObjectMapperConfiguration.java
@@ -0,0 +1,26 @@
+package com.gitlab.amirmehdi.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+@Configuration
+public class ObjectMapperConfiguration {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Tehran"));
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+        return objectMapper;
+    }
+}
