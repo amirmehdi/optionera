4906c9800e7808b6aeac6820efa5ffc3e60f45fe
alidarvishi14 <alidarvishi14@gmail.com>
2020-10-27 03:48:08 +0330

    valuablePrice hot fix

--- src/main/java/com/gitlab/amirmehdi/service/calculator/ValuablePrice.java
+++ src/main/java/com/gitlab/amirmehdi/service/calculator/ValuablePrice.java
@@ -8,8 +8,8 @@ public class ValuablePrice {
     private static final double lastToCloseWeight = 0.2;
 
     public static long calc(int closePrice, int lastPrice, int refPrice, int maxPrice) {
-        double dynamicThreshold = staticThreshold1 + (lastPriceWeight * Math.pow(1.0 * lastPrice / refPrice - 1, 5) + closePriceWeight * (1.0 * closePrice / refPrice - 1) * 5) /
-            ((1.0 * maxPrice / refPrice - 1) * 5);
+        double dynamicThreshold = staticThreshold1 + ( lastPriceWeight * Math.pow(1.0 * lastPrice / refPrice - 1, 5)
+            + closePriceWeight * Math.pow(1.0 * closePrice / refPrice - 1 , 5) ) / Math.pow((1.0 * maxPrice / refPrice - 1) , 5);
         double valuablePrice1 = Math.round((closePrice + lastToCloseWeight * lastPrice) / ((lastToCloseWeight + 1) * (1 + dynamicThreshold / 100)));
         double valuablePrice2 = Math.round(lastPrice * (1 + staticThreshold2 / 100));
         return (long) Math.min(valuablePrice1, valuablePrice2);
