ec1cc9f1d7cd7a94ba4652ff8942187e5a1789ed
oa-user <amirmehdinaghavi@gmail.com>
2020-11-02 01:30:56 +0330

    add LegalSupplyStrategy

--- src/main/java/com/gitlab/amirmehdi/repository/BoardRepository.java
+++ src/main/java/com/gitlab/amirmehdi/repository/BoardRepository.java
@@ -3,7 +3,10 @@ package com.gitlab.amirmehdi.repository;
 import com.gitlab.amirmehdi.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data  repository for the Board entity.
@@ -11,4 +14,10 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface BoardRepository extends JpaRepository<Board, String>, JpaSpecificationExecutor<Board> {
+
+    @Query(value = "select b " +
+        " from Board b where b.isin like '%IRO9' " +
+        "               and 2*b.legalSellVolume>b.tradeVolume " +
+        "               and b.tradeValue>10000000")
+    List<Board> findAllForLegalSupplyStrategy();
 }
---
+++ src/main/java/com/gitlab/amirmehdi/service/strategy/LegalSupplyStrategy.java
@@ -0,0 +1,54 @@
+package com.gitlab.amirmehdi.service.strategy;
+
+import com.gitlab.amirmehdi.repository.BoardRepository;
+import com.gitlab.amirmehdi.repository.OptionRepository;
+import com.gitlab.amirmehdi.service.Market;
+import com.gitlab.amirmehdi.service.OptionStatsService;
+import com.gitlab.amirmehdi.service.dto.StrategyResponse;
+import net.jodah.expiringmap.ExpiringMap;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class LegalSupplyStrategy extends Strategy {
+    private final BoardRepository boardRepository;
+    private final Map<String, Float> cachedIsin = ExpiringMap.builder()
+        .expirationPolicy(ExpiringMap.ExpirationPolicy.CREATED)
+        .expiration(1, TimeUnit.HOURS)
+        .build();
+
+    protected LegalSupplyStrategy(OptionRepository optionRepository, OptionStatsService optionStatsService, Market market, BoardRepository boardRepository) {
+        super(optionRepository, optionStatsService, market);
+        this.boardRepository = boardRepository;
+    }
+
+    @Override
+    public StrategyResponse getSignals() {
+        return StrategyResponse.builder()
+            .callSignals(boardRepository.findAllForLegalSupplyStrategy()
+                .stream()
+                .filter(board -> !cachedIsin.containsKey(board.getIsin()))
+                .peek(board -> cachedIsin.put(board.getIsin(), (float) (board.getLegalBuyVolume()) / board.getTradeVolume()))
+                .map(board -> getSignal(board.getIsin()))
+                .collect(Collectors.toList()))
+            .publicChatId(optionEraChatId)
+            .sendOrderType(StrategyResponse.SendOrderType.NONE)
+            .build();
+    }
+
+    @Override
+    protected String getStrategyDesc() {
+        return "عرضهـحقوقی حقوقی درحال عرضه است";
+    }
+
+    @Override
+    protected String getStrategyRisk() {
+        return "متوسط";
+    }
+
+    @Override
+    public String getCron() {
+        return "30 * 9-12 * * *";
+    }
+}
