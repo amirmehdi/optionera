3d8c058383b75f6475752fa359218f0817e36687
oa-user <amirmehdinaghavi@gmail.com>
2020-11-08 09:14:59 +0330

    fix

--- src/main/webapp/app/modules/HomePage/HomePage.tsx
+++ src/main/webapp/app/modules/HomePage/HomePage.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Row} from 'reactstrap';
-import  OptionStats  from 'app/entities/option-stats/option-stats';
-import { RouteComponentProps } from 'react-router-dom';
+import {connect} from 'react-redux';
+import OptionStats from 'app/entities/option-stats/option-stats';
+import {RouteComponentProps} from 'react-router-dom';
 
 export type IHomeProp = StateProps;
 export interface IOptionStatsProps extends StateProps, RouteComponentProps<{ url: string }> {
@@ -12,8 +11,8 @@ export const HomePage = (props: IOptionStatsProps) => {
   const { account } = props;
   return (
     <div style={{padding: 10}}>
-      {account.authorities && account.authorities[0] === "ROLE_USER" ?
-        account.authorities[1] !== "ROLE_ADMIN" ?
+      {account.authorities && account.authorities.includes("ROLE_USER") ?
+        !account.authorities.includes("ROLE_ADMIN") ?
         <OptionStats  history={props.history}
                       location={props.location}
                       match={props.match} /> : "" : ""
