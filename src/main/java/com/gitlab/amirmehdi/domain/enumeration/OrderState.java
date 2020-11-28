91a49e46296cffe0acc85f9931e3f6848f8c7ce4
amirmehdi <amirmehdinaghavi@gmail.com>
2020-11-28 08:28:34 +0330

    file missing

---
+++ src/main/java/com/gitlab/amirmehdi/domain/enumeration/OrderState.java
@@ -0,0 +1,8 @@
+package com.gitlab.amirmehdi.domain.enumeration;
+
+/**
+ * The State enumeration.
+ */
+public enum OrderState {
+    NONE, ACTIVE, EXECUTED, CANCELLED, ERROR, PARTIALLY_EXECUTED
+}
