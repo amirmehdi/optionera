e84fce73ecfbf273f6b8b629b58a32d7e59af9ee
amirmehdi <amirmehdinaghavi@gmail.com>
2020-12-19 16:52:20 +0330

    fix entrypoint.sh

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-exec apt-get update
+apt-get update
 echo "install imagemagick and tesseract as dependencies"
-exec apt-get -y install libjpeg-dev libpng-dev tesseract-ocr imagemagick
+apt-get -y install libjpeg-dev libpng-dev tesseract-ocr imagemagick
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.gitlab.amirmehdi.ETradeApp"  "$@"
