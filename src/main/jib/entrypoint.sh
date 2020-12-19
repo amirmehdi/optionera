e1b5591aa851bf0cb90ec91bd28d7558f5fe38f4
amirmehdi <amirmehdinaghavi@gmail.com>
2020-12-19 16:27:53 +0330

    install dependencies

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-
+echo "install imagemagick and tesseract as dependencies"
+exec apt update && apt -y install libjpeg-dev libpng-dev tesseract-ocr imagemagick
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.gitlab.amirmehdi.ETradeApp"  "$@"
