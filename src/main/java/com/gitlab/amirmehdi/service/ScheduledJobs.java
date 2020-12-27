d3bd19ae0adf8007ef5e59acb78c8871f6dc6422
amirmehdi <amirmehdinaghavi@gmail.com>
2020-12-28 00:21:40 +0330

    HeadLineAlgorithm added

--- src/main/java/com/gitlab/amirmehdi/service/ScheduledJobs.java
+++ src/main/java/com/gitlab/amirmehdi/service/ScheduledJobs.java
@@ -1,37 +1,26 @@
 package com.gitlab.amirmehdi.service;
 
 import com.gitlab.amirmehdi.config.ApplicationProperties;
-import com.gitlab.amirmehdi.domain.Order;
-import com.gitlab.amirmehdi.domain.enumeration.OrderState;
 import com.gitlab.amirmehdi.service.crawler.CrawlerBox;
 import com.gitlab.amirmehdi.service.crawler.TseCrawler;
 import com.gitlab.amirmehdi.service.dto.TelegramMessageDto;
 import com.gitlab.amirmehdi.util.MarketTimeUtil;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.List;
 
 @Service
 @Log4j2
 public class ScheduledJobs {
     private final TseCrawler tseCrawler;
     private final CrawlerBox crawlerBox;
-    private final OrderService orderService;
-    private final TaskScheduler executor;
     private final Market market;
     private final TelegramMessageSender telegramMessageSender;
     private final ApplicationProperties properties;
 
-    public ScheduledJobs(TseCrawler tseCrawler, CrawlerBox crawlerBox, OrderService orderService, TaskScheduler executor, Market market, TelegramMessageSender telegramMessageSender, ApplicationProperties applicationProperties) {
+    public ScheduledJobs(TseCrawler tseCrawler, CrawlerBox crawlerBox, Market market, TelegramMessageSender telegramMessageSender, ApplicationProperties applicationProperties) {
         this.tseCrawler = tseCrawler;
         this.crawlerBox = crawlerBox;
-        this.orderService = orderService;
-        this.executor = executor;
         this.market = market;
         this.telegramMessageSender = telegramMessageSender;
         this.properties = applicationProperties;
@@ -79,28 +68,6 @@ public class ScheduledJobs {
             crawlerBox.getBestMarketUpdater().boardUpdater();
             crawlerBox.getBestMarketUpdater().instrumentUpdater();
             telegramMessageSender.sendMessage(new TelegramMessageDto(properties.getTelegram().getHealthCheckChat(), "Why redis is empty!"));
-        }
-    }
-
-    @Scheduled(cron = "${application.headline.cron}")
-    public void headLineOrder() {
-        log.info("headLineOrder fired");
-        List<Order> allByState = orderService.findAllByState(OrderState.HEADLINE);
-        //TODO max price and min price by stockwatch
-        for (Order order : allByState) {
-            for (int i = 0; i < properties.getHeadline().getRepeat(); i++) {
-                executor.schedule(() -> orderService.sendOrder(
-                    new Order()
-                        .broker(order.getBroker())
-                        .isin(order.getIsin())
-                        .price(order.getPrice())
-                        .quantity(order.getQuantity())
-                        .validity(order.getValidity())
-                        .side(order.getSide()))
-                    , new Date()
-                        .toInstant()
-                        .plus(i * properties.getHeadline().getSleep(), ChronoUnit.MILLIS));
-            }
         }
     }
 }
---
+++ src/main/java/com/gitlab/amirmehdi/service/algorithm/HeadLineAlgorithm.java
@@ -0,0 +1,57 @@
+package com.gitlab.amirmehdi.service.algorithm;
+
+import com.gitlab.amirmehdi.config.ApplicationProperties;
+import com.gitlab.amirmehdi.domain.Order;
+import com.gitlab.amirmehdi.domain.enumeration.OrderState;
+import com.gitlab.amirmehdi.service.Market;
+import com.gitlab.amirmehdi.service.OrderService;
+import com.gitlab.amirmehdi.service.dto.core.StockWatch;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Log4j2
+public class HeadLineAlgorithm {
+    private final OrderService orderService;
+    private final ApplicationProperties properties;
+    private final Market market;
+    private final TaskScheduler executor;
+
+    public HeadLineAlgorithm(OrderService orderService, ApplicationProperties properties, Market market, TaskScheduler executor) {
+        this.orderService = orderService;
+        this.properties = properties;
+        this.market = market;
+        this.executor = executor;
+    }
+
+
+    @Scheduled(cron = "${application.headline.cron}")
+    public void headLineOrder() {
+        log.info("headLineOrder fired");
+        List<Order> allByState = orderService.findAllByState(OrderState.HEADLINE);
+        for (Order order : allByState) {
+            StockWatch stockWatch = market.getStockWatch(order.getIsin());
+            int price = stockWatch==null?order.getPrice():stockWatch.getMax();
+            int quantity = (int) (order.getBourseCode().getBuyingPower()/(2*price));
+            for (int i = 0; i < properties.getHeadline().getRepeat(); i++) {
+                executor.schedule(() -> orderService.sendOrder(
+                    new Order()
+                        .broker(order.getBroker())
+                        .isin(order.getIsin())
+                        .price(price)
+                        .quantity(quantity)
+                        .validity(order.getValidity())
+                        .side(order.getSide()))
+                    , new Date()
+                        .toInstant()
+                        .plus(i * properties.getHeadline().getSleep(), ChronoUnit.MILLIS));
+            }
+        }
+    }
+}
