959b594e93705ee49f8196e3f442aa883d61d1a1
amirmehdi <amirmehdinaghavi@gmail.com>
2020-12-30 03:54:32 +0330

    wtf

--- src/main/java/com/gitlab/amirmehdi/service/asa/AsaRLCConsumer.java
+++ src/main/java/com/gitlab/amirmehdi/service/asa/AsaRLCConsumer.java
@@ -1,5 +1,7 @@
 package com.gitlab.amirmehdi.service.asa;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gitlab.amirmehdi.service.dto.asa.InstrumentInfoResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpEntity;
@@ -17,23 +19,29 @@ import java.util.concurrent.CompletableFuture;
 public class AsaRLCConsumer {
 
     private final RestTemplate restTemplate;
-
+    private final ObjectMapper objectMapper;
     private final String instrumentInfoUrl = "https://online.agah.com/Watch/GetInstrumentInfo?isin=%s";
 
-    public AsaRLCConsumer(RestTemplate restTemplate) {
+    public AsaRLCConsumer(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @Async
     public CompletableFuture<InstrumentInfoResponse> getInstrumentInfo(String isin, String token) {
         StopWatch watch = new StopWatch("getInstrumentInfo");
         watch.start();
-        InstrumentInfoResponse response = restTemplate.exchange(
-            String.format(instrumentInfoUrl, isin)
-            , HttpMethod.GET
-            , new HttpEntity<>(getHeader(token))
-            , InstrumentInfoResponse.class)
-            .getBody();
+        InstrumentInfoResponse response = null;
+        try {
+            response = objectMapper.readValue(restTemplate.exchange(
+                String.format(instrumentInfoUrl, isin)
+                , HttpMethod.GET
+                , new HttpEntity<>(getHeader(token))
+                , String.class)
+                .getBody(), InstrumentInfoResponse.class);
+        } catch (JsonProcessingException e) {
+            log.error(e.toString());
+        }
         watch.stop();
         log.debug(watch.shortSummary());
         return CompletableFuture.completedFuture(response);
@@ -45,6 +53,7 @@ public class AsaRLCConsumer {
         LinkedMultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
         headers.add("User-Agent", s[0]);
         headers.add("Cookie", s[1]);
+        headers.add("Content-Type", "application/json; charset=UTF-8");
         return headers;
     }
 
